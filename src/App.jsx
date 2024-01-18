74da5a19b9cbb2695c62ed0cdd0932712023f254
renehellmich <renehellmich94@gmail.com>
2024-01-18 11:34:53 +0100

    stage 1 with basic functions completed

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import './App.css'
+import MovieContainer from './assets/components/movieContainer/MovieContainer'
+import movies from './assets/data/data'
+
+function App() {
+
+  console.log(movies);
+  return (
+    <>
+      <header>
+
+      </header>
+      <main>
+        {
+          movies.map((movie, index) => {
+            return (
+              <div key={index}>
+                <MovieContainer
+                  title={movie.title}
+                  director={movie.director}
+                  year={movie.year}
+                  duration={movie.duration}
+                  genre={movie.genre}
+                  rate={movie.rate}
+                />
+              </div>
+            )
+          })
+        }
+      </main>
+    </>
+  )
+}
+
+export default App
---
+++ src/assets/components/movieContainer/MovieContainer.jsx
@@ -0,0 +1,24 @@
+import GenreDiv from "../genreDiv/GenreDiv";
+
+const MovieContainer = ({ title, director, year, duration, genre, rate }) => {
+    return (
+        <>
+            <h4>{title}</h4>
+            <p className="movieDescription">{year}</p>
+            <p className="movieDescription">{director}</p>
+            <p className="movieDescription">{duration}</p>
+            <p className="movieDescription">{rate}</p>
+            <ul>{genre.map((object, index) => {
+                return (
+                    <div key={index}>
+                        <GenreDiv
+                            object={object}
+                        />
+                    </div>
+                )
+            })}</ul>
+        </>
+    );
+}
+
+export default MovieContainer;
